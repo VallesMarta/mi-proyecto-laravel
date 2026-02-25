2329b7c2f15c21a35440f01f7ef776f173e494dd
Marta Vallés <mvallesterol12@gmail.com>
2026-02-25 11:25:24 +0100

    Actualizamos tabulacion en ficheros

--- src/app/Http/Controllers/ViajeController.php
+++ src/app/Http/Controllers/ViajeController.php
@@ -6,26 +6,15 @@ use Illuminate\Http\Request;
 
 class ViajeController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         $viajes = Viaje::all();
         return view('viajes.index', compact('viajes'));
     }
-
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         return view('viajes.create');
     }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         $viaje = new Viaje();
@@ -36,31 +25,17 @@ class ViajeController extends Controller
         $viaje->descripcion = $request->input('descripcion');
         $viaje->enlace = $request->input('enlace');
         $viaje->save();
-
-        // Redirigir a la lista de notas
         return redirect()->route('viaje.index');
     }
-
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
         $viaje = Viaje::findOrFail($id);
         return view('viajes.show', compact('viaje'));
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(Viaje $viaje)
     {
         return view('viajes.edit', compact('viaje'));
     }
-
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id)
     {
         
@@ -68,10 +43,6 @@ class ViajeController extends Controller
         $viaje->update($request->all());
         return redirect()->route('viaje.index');
     }
-
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
         $viaje = Viaje::findOrFail($id);
